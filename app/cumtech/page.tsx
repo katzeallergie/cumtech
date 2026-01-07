8e0e55d4d90736921ba0608f9f847b2cb326933f
katzeallergie <tatsuhiko_saito@r.recruit.co.jp>
2026-01-07 23:47:38 +0900

    hoge

--- app/cumtech/page.tsx
+++ app/cumtech/page.tsx
@@ -38,7 +38,6 @@ export default function Home() {
             <Button variant="bordered">Open Menu</Button>
           </DropdownTrigger>
           <DropdownMenu aria-label="Static Actions">
-            <DropdownItem key="new">New file</DropdownItem>
             <DropdownItem key="copy">Copy link</DropdownItem>
             <DropdownItem key="edit">Edit file</DropdownItem>
             <DropdownItem key="delete" className="text-danger" color="danger">
